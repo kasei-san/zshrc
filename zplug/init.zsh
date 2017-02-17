0e3cb967a7b960e01bfb644f3e9f6280ab684c8e
kasei_san <kasei.san@gmail.com>
2017-02-17 17:42:36 +0900

    Unuse enhancd

--- zplug/init.zsh
+++ zplug/init.zsh
@@ -1,6 +1,6 @@
 #--------------------------------------------------------------------------
 # enhancd {{{
 #--------------------------------------------------------------------------
-export ENHANCD_FILTER=percol
-zplug "b4b4r07/enhancd", use:init.sh
+# export ENHANCD_FILTER=percol
+# zplug "b4b4r07/enhancd", use:init.sh
 # }}}
